d40c40c267a3b1677db84e36f441047380c24399
Bradley Momberger <brad@bitovi.com>
2017-09-29 19:44:56 -0400

    Initial commit

---
+++ detect-cycles-codemod.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
+
+
+module.exports = {
+	getOptions: function () {
+		return [];
+	},
+
+	run: function (directory, options) {
+		var packageJsonPath = path.join(directory, 'package.json');
+		var packageJson = require(packageJsonPath);
+
+		// We've read in package.json into an object.  See if the detect-cycle script exists on the scripts object
+		//  if not, add it to the scripts and edit the preversion script to run it.
+		if(!packageJson.scripts["detect-cycle"]) {
+			packageJson.scripts["detect-cycle"] = "detect-cyclic-packages";
+			if(!~packageJson.scripts.test.indexOf("detect-cycle")) {
+				packageJson.scripts.test = "npm run detect-cycle && " + packageJson.scripts.test;
+			}
+		}
+
+		// Write the edited package.json file out, resolve a promise when finished.
+		var writePackageJson = new Promise(function (resolve, reject) {
+			fs.writeFile(packageJsonPath, JSON.stringify(packageJson), function (error) {
+				error ? reject(error) : resolve();
+			});
+		});
+
+		// Install nocycle as a dev dependency.  Resove a Promise when finished.
+		var installs = new Promise(function(resolve, reject) {
+			exec("npm install --save-dev detect-cyclic-packages", function(error) {
+					error ? reject(error) : resolve();
+			});
+		});
+
+		// Return control to landscaper when everything finishes or something fails.
+		return Promise.all([writePackageJson, installs]).catch(function(err) {
+			console.error(err);
+		});
+	}
+};
+
---
+++ readme.md
@@ -0,0 +1,20 @@
+# detect-cycles-codemod
+
+[![Join the chat at https://gitter.im/canjs/canjs](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/canjs/canjs?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+[![License: MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/canjs/can-connect/blob/master/LICENSE.md)
+[![npm version](https://badge.fury.io/js/detect-cycles-codemod.svg)](https://www.npmjs.com/package/detect-cycles-codemod)
+[![Greenkeeper badge](https://badges.greenkeeper.io/canjs/detect-cycles-codemod.svg)](https://greenkeeper.io/)
+
+`detect-cycles-codemod` is a Landscaper task to add cyclic dependency checking to your package.
+
+## Changelog
+
+See the [latest releases on GitHub](https://github.com/canjs/detect-cycles-codemod/releases).
+
+## Contributing
+
+The [contribution guide](https://github.com/canjs/detect-cycles-codemod/blob/master/CONTRIBUTING.md) has information on getting help, reporting bugs, developing locally, and more.
+
+## License
+
+[MIT](https://github.com/canjs/detect-cycles-codemod/blob/master/LICENSE.md)
